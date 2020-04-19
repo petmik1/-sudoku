faf7c59140d8a6cda339822111a7f5429de8fcde
petmik1 <petter.mikalsen.pehm@gmail.com>
2020-04-19 17:13:44 +0200

    v3

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log('hello world')
+var rad1rute1 = document.getElementById('rad1rute1');
+var button = document.getElementById('button')
+button.onsubmit = function (event) {
+  event.preventdefault();
+  console.log(rad1rute1.value);
+
+}
