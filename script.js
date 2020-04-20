f27ac8cce5bbdac77f7a8d86861a128df4e65515
petmik1 <petter.mikalsen.pehm@gmail.com>
2020-04-20 11:50:00 +0200

    v4

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ button.onsubmit = function (event) {
   console.log(rad1rute1.value);
 
 }
+
+puzzle = [[0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0]]
+// https://www.valentinog.com/blog/html-table/
+
+
+// function tableCreate() {
+//   var body = document.getElementsByTagName('body')[0];
+//   var tbl = document.createElement('table');
+//   tbl.style.width = '100%';
+//   tbl.setAttribute('border', '1');
+//   var tbdy = document.createElement('tbody');
+//   for (var i = 0; i < 3; i++) {
+//     var tr = document.createElement('tr');
+//     for (var j = 0; j < 2; j++) {
+//       if (i == 2 && j == 1) {
+//         break
+//       } else {
+//         var td = document.createElement('td');
+//         td.appendChild(document.createTextNode('\u0
