6d5f555ad16038477a1001fa0d4fc0f936b98361
petmik1 <petter.mikalsen.pehm@gmail.com>
2020-04-22 20:35:44 +0200

    Co-authored-by: Thunder-Lion <leondammann01@gmail.com> Co-authored-by: ali200117 <ali_kadri2016@hotmail.com> Co-authored-by: sunniva2001 <sunniva.alvern@hotmail.no>

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
 console.log('hello world')
-   document.write("<table border='1px' class='table'>");
-            
-            
-            
-            // for(i = 1; i < 11; i++)
-            // {
-            //     document.write("<th> [ " + i + " ]</th>");
-            // }
-            
-           
-            
-            for(x = 1; x<10; x++) {
-               
-                
-                document.write("<tr>");
-                
-                for(y= 1; y< 10; y++) {
-                 document.write("<td>"+'<input type="number" id=''  >'+"</td>");
-                }
-                
-                 document.write("</tr>"); 
-            }
-
-            document.write("</table>");
+document.write("<table border='1px' class='table'>");
 
 
+
+// for(i = 1; i < 11; i++)
+// {
+//     document.write("<th> [ " + i + " ]</th>");
+// }
+
+
+
+for (x = 1; x < 10; x++) {
+
+
+    document.write("<tr>");
+
+    for (y = 1; y < 10; y++) {
+        document.write("<  d>" + "<input type ='number' max ='9' min='0' >" + "</td>");
+    }
+
+    document.write("</tr>");
+}
+
+document.write("</table>");
 
 
 
 
-// puzzle = [
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0]
-// ]
+
+
+/*puzzle = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0]
+]*/
