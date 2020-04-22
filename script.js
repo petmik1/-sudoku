0a1ac01157e4a2cba3c2670a4c8d5f5e726553c6
petmik1 <petter.mikalsen.pehm@gmail.com>
2020-04-22 21:19:22 +0200

    posisjonert table

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-console.log('hello world')
-document.write("<table border='1px' class='table'>");
-
-
-
-// for(i = 1; i < 11; i++)
-// {
-//     document.write("<th> [ " + i + " ]</th>");
-// }
-
-
+document.write("<table border='1px' class='table align='center'>");
 
 for (x = 1; x < 10; x++) {
 
@@ -16,14 +6,13 @@ for (x = 1; x < 10; x++) {
     document.write("<tr>");
 
     for (y = 1; y < 10; y++) {
-        document.write("<  d>" + "<input type ='number' max ='9' min='0' >" + "</td>");
+        document.write("<td>" + "<input type ='number'  class='inptbl' max ='9' min='0' value='0'>" + "</td>");
     }
 
     document.write("</tr>");
 }
 
 document.write("</table>");
-
 
 
 
