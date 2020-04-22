f847fa59360d336e5d6165b233786de8bb32337a
petmik1 <petter.mikalsen.pehm@gmail.com>
2020-04-22 10:10:14 +0200

    fikk inn grid

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 console.log('hello world')
-   document.write("<table border='1px'>");
+   document.write("<table border='1px' class='table'>");
             
             
             
-            for(i = 1; i < 11; i++)
-            {
-                document.write("<th> [ " + i + " ]</th>");
-            }
+            // for(i = 1; i < 11; i++)
+            // {
+            //     document.write("<th> [ " + i + " ]</th>");
+            // }
             
            
             
-            for(k = 1; k<11; k++)
-            {
+            for(x = 1; x<10; x++) {
                
                 
                 document.write("<tr>");
                 
-                for(j= 1; j< 11; j++)
-                {
-                 document.write("<td>"+j+" X "+ k + " = " + j*k + "</td>")   
+                for(y= 1; y< 10; y++) {
+                 document.write("<td>"+'<input type="number" id=''  >'+"</td>");
                 }
                 
                  document.write("</tr>"); 
@@ -42,26 +40,3 @@ console.log('hello world')
 //   [0, 0, 0, 0, 0, 0, 0, 0, 0],
 //   [0, 0, 0, 0, 0, 0, 0, 0, 0]
 // ]
-
-// function tableCreate() {
-  
-
-// }
-// tableCreate();
-// https://www.valentinog.com/blog/html-table/
-
-
-// function tableCreate() {
-//   var body = document.getElementsByTagName('body')[0];
-//   var tbl = document.createElement('table');
-//   tbl.style.width = '100%';
-//   tbl.setAttribute('border', '1');
-//   var tbdy = document.createElement('tbody');
-//   for (var i = 0; i < 3; i++) {
-//     var tr = document.createElement('tr');
-//     for (var j = 0; j < 2; j++) {
-//       if (i == 2 && j == 1) {
-//         break
-//       } else {
-//         var td = document.createElement('td');
-//         td.appendChild(document.createTextNode('\u0
