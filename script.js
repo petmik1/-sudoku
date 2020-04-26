b069b2f91bd79d6e66187a5412d71684cf182d8a
petmik1 <petter.mikalsen.pehm@gmail.com>
2020-04-26 19:26:45 +0200

    fikset posisjonering tabell

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-document.write("<form>")
+//lager tabell med inputfelt
+document.write("<form>");
 document.write("<table border='1px' class='table align='center'>");
 
 for (x = 1; x < 10; x++) {
+  document.write("<tr>");
 
+  for (y = 1; y < 10; y++) {
+    document.write(
+      "<td>" +
+        "<input type ='number'  class='inptbl' max ='9' min='0' value='0'>" +
+        "</td>"
+    );
+  }
 
-    document.write("<tr>");
-
-    for (y = 1; y < 10; y++) {
-        document.write("<td>" + "<input type ='number'  class='inptbl' max ='9' min='0' value='0'>" + "</td>");
-    }
-
-    document.write("</tr>");
+  document.write("</tr>");
 }
 
 document.write("</table>");
-document.write("</form>")
+document.write("</form>");
 
 function myfunction() {
-
+  console.log("hello world");
 }
-
-
 
 /*puzzle = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
